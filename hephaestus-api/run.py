d06997d028602e23963cefda148f70b4c2760770
Bartosz Stopa <stopa323@gmail.com>
2020-01-13 21:36:29 +0100

    Add mongodb

--- hephaestus-api/run.py
+++ hephaestus-api/run.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_pymongo import PyMongo
 
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://127.0.0.1:27017/hephaestus_db"
+mongo = PyMongo(app)
 
 
 @app.route("/")
 def index():
-    return "Seems fine"
+    a = mongo.db.blueprints.find()
+    res = []
+    for x in a:
+        res.append(x['key'])
+
+    return jsonify(res)
 
 
 if __name__ == "__main__":
