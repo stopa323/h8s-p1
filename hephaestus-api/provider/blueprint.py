f1f456790ed24a2c420bd777f48a6ed4e5a7e68f
Bartosz Stopa <stopa323@gmail.com>
2020-01-22 22:07:19 +0100

    Add blueprint get all endpoint

--- hephaestus-api/provider/blueprint.py
+++ hephaestus-api/provider/blueprint.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from common.db import get_client
 from schema import Blueprint, BlueprintCreate
 
@@ -9,3 +11,12 @@ def create_blueprint(bp: BlueprintCreate) -> Blueprint:
     data = bp.save()
     item = Blueprint(**data)
     return item
+
+
+def get_blueprint_list() -> List[Blueprint]:
+    items = list()
+    for bp in db.blueprints.find({}):
+        bp["id"] = bp["_id"]
+        items.append(Blueprint(**bp))
+
+    return items
--- hephaestus-api/router/blueprint.py
+++ hephaestus-api/router/blueprint.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from typing import List
 
 from provider import blueprint
 from schema import Blueprint, BlueprintCreate
@@ -8,6 +9,12 @@ router = APIRouter()
 
 
 @router.post("/", response_model=Blueprint)
-async def create_blueprint(bp: BlueprintCreate) -> Blueprint:
+async def create_blueprint(bp: BlueprintCreate):
     item = blueprint.create_blueprint(bp)
     return item
+
+
+@router.get("/", response_model=List[Blueprint])
+async def get_blueprint_list():
+    items = blueprint.get_blueprint_list()
+    return items
