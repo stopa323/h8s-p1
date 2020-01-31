b056c7e74bd54f26007429ac68b1f17c075cb910
Bartosz Stopa <stopa323@gmail.com>
2020-02-01 00:25:16 +0100

    Add description for blueprint endpoints

--- hephaestus-bp/router/blueprint.py
+++ hephaestus-bp/router/blueprint.py
@@ -8,13 +8,20 @@ from model.blueprint import BlueprintCreate, BlueprintObj
 router = APIRouter()
 
 
-@router.post("/blueprints", response_model=BlueprintObj)
+@router.post("/blueprints",
+             response_model=BlueprintObj,
+             name="Create new blueprint",
+             description="Initialize new blueprint document. Always create with"
+                         " entry and exit nodes")
 async def create_blueprint(bp: BlueprintCreate):
     item = blueprint.create_blueprint(bp)
     return item
 
 
-@router.get("/blueprints", response_model=List[BlueprintObj])
+@router.get("/blueprints",
+            response_model=List[BlueprintObj],
+            name="Get blueprint documents",
+            description="Fetch all defined blueprint documents.")
 async def get_blueprint_list():
     items = blueprint.get_blueprint_list()
     return items
